b81c5fe660641ed8b5e14a06fc358e1bf65de064
Nicolas-Rodrigues1 <nicolas.rodrigues@ds2u.com.br>
2024-11-07 15:47:46 -0300

    testando lib link.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DetachedModule } from 'ds2u-lib';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DetachedModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
